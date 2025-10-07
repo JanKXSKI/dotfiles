15f0d62b63124d3a0960adafe7b71d93c5c7fb77
Jan Klimaschewski <Jan.Klimaschewski@profidata.com>
2025-10-07 09:54:43 +0200

    update

--- dot_sh/logging.sh
+++ dot_sh/logging.sh
@@ -1,5 +1,4 @@
-logdir=$1
-parent=$(basename "$(ps -o comm= $PPID)")
-exec {BASH_XTRACEFD}>$logdir/log-${0##*/}.$parent.log
+logdir="$1"
+parent="$(basename "$(ps -o comm= $PPID)")"
+exec {BASH_XTRACEFD}>"$logdir/log-${0##*/}.$parent.log"
 set -x
-
