5120004e05fda84cee578f14bbddc75d22f9445b
Jan Klimaschewski <Jan.Klimaschewski@profidata.com>
2025-10-09 13:05:12 +0200

    logging with KLJA_LOGGING

--- dot_sh/logging.sh
+++ dot_sh/logging.sh
@@ -1,5 +1,7 @@
-logdir="$1"
-parentProcess=$(ps -o comm= $PPID)
-parent="$(basename "${parentProcess#-}")"
-exec {BASH_XTRACEFD}>"$logdir/log-${0##*/}.$parent.log"
-set -x
+if [[ -n $KLJA_LOGGING ]]; then
+    logdir="$1"
+    parentProcess=$(ps -o comm= $PPID)
+    parent="$(basename "${parentProcess#-}")"
+    exec {BASH_XTRACEFD}>"$logdir/log-${0##*/}.$parent.log"
+    set -x
+fi
