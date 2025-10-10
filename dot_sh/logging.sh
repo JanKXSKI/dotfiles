86f4c7257ec04a16e9a0ada7c44a918312a141af
Jan Klimaschewski <Jan.Klimaschewski@profidata.com>
2025-10-10 18:13:07 +0200

    always log

--- dot_sh/logging.sh
+++ dot_sh/logging.sh
@@ -1,10 +1,8 @@
-if [[ -n $KLJA_LOGGING ]]; then
-    logdir="$1"
-    parentProcess=$(ps -o comm= $PPID)
-    parent="$(basename "${parentProcess#-}")"
-    log="$logdir/log-${0##*/}.$parent.log"
-    export PS4='\033[0D$(printf "%-15.15s" "$(date "+%H:%M:%S %N")") '
-    exec 2>"$log"
-    exec {BASH_XTRACEFD}>>"$log"
-    set -x
-fi
+logdir="$1"
+parentProcess=$(ps -o comm= $PPID)
+parent="$(basename "${parentProcess#-}")"
+log="$logdir/log-${0##*/}.$parent.log"
+export PS4='\033[0D$(printf "%-15.15s" "$(date "+%H:%M:%S %N")") '
+exec 2>"$log"
+exec {BASH_XTRACEFD}>>"$log"
+set -x
