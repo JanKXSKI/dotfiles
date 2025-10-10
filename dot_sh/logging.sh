deaca2d84ed525eba0c9477d4e3b21fab03205c0
Jan Klimaschewski <Jan.Klimaschewski@profidata.com>
2025-10-10 16:58:33 +0200

    little fix

--- dot_sh/logging.sh
+++ dot_sh/logging.sh
@@ -3,8 +3,8 @@ if [[ -n $KLJA_LOGGING ]]; then
     parentProcess=$(ps -o comm= $PPID)
     parent="$(basename "${parentProcess#-}")"
     log="$logdir/log-${0##*/}.$parent.log"
-    date >$log
-    exec 2>>$log
-    exec {BASH_XTRACEFD}>>$log
+    date >"$log"
+    exec 2>>"$log"
+    exec {BASH_XTRACEFD}>>"$log"
     set -x
 fi
