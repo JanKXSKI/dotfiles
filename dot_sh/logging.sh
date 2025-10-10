c5bef0186f5dba4c0b1a5aa09ca5e736841f651a
Jan Klimaschewski <Jan.Klimaschewski@profidata.com>
2025-10-10 17:27:25 +0200

    fix tmux windows

--- dot_sh/logging.sh
+++ dot_sh/logging.sh
@@ -3,8 +3,8 @@ if [[ -n $KLJA_LOGGING ]]; then
     parentProcess=$(ps -o comm= $PPID)
     parent="$(basename "${parentProcess#-}")"
     log="$logdir/log-${0##*/}.$parent.log"
-    date >"$log"
-    exec 2>>"$log"
+    export PS4='\033[0D$(printf "%-15.15s" "$(date "+%H:%M:%S %N")") '
+    exec 2>"$log"
     exec {BASH_XTRACEFD}>>"$log"
     set -x
 fi
